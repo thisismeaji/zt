d946162e751775207fbc37fa1fcdacfa545d70d5
Ajisaka Kamandanu <thisismeaji20@gmail.com>
2024-10-20 16:06:48 +0700

    add interactive on navigation

--- src/components/navigation/Navigation.js
+++ src/components/navigation/Navigation.js
@@ -1,8 +1,21 @@
+'use client';
+
+import { useState } from 'react'; // Import useState
 import Styles from "../navigation/navigation.module.css";
 import Link from 'next/link';
 
 export default function Navigation() {
-    return(
+    const [isNavActive, setIsNavActive] = useState(false); // State untuk menangani kelas
+
+    const handleMenuClick = () => {
+        setIsNavActive(prev => !prev); // Toggle state saat menu diklik
+    };
+
+    const handleLinkClick = () => {
+        setIsNavActive(false); // Menutup navMobile saat link diklik
+    };
+
+    return (
         <nav className={Styles.nav}>
             <div className={Styles.logo}>
                 <p>ZonaTenang</p>
@@ -17,21 +30,25 @@ export default function Navigation() {
                     </li>
                 </ul>
             </div>
-            <div className={Styles.menu}>
+            <div className={Styles.menu} onClick={handleMenuClick}>
                 <span></span>
                 <span></span>
                 <span></span>
             </div>
-            <div className={Styles.navMobile}>
+            <div className={`${Styles.navMobile} ${isNavActive ? Styles.navActive : ''}`}>
+                <div>
+                    <p>Menu</p>
+                </div>
                 <ul>
                     <li>
-                        <Link href="/">Beranda</Link>
+                        <Link href="/" onClick={handleLinkClick}>Beranda</Link> {/* Tambahkan onClick di sini */}
                     </li>
                     <li>
-                        <Link href="/tips-dan-trik">Tips dan Trik</Link>
+                        <Link href="/tips-dan-trik" onClick={handleLinkClick}>Tips dan Trik</Link> {/* Tambahkan onClick di sini */}
                     </li>
                 </ul>
             </div>
+            {isNavActive && <div className={Styles.backdrop} onClick={handleMenuClick} />} {/* Layer untuk blur */}
         </nav>
     );
 }
